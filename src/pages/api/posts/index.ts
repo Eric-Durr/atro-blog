9b17ad8ba7e603170305c8046cff77650e0a61c7
Eric Durr Sierra <ericdurrsierra@Erics-MacBook-Pro.local>
2026-03-06 10:40:11 +0000

    use query params: server mode is mandatory

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,15 +1,37 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 
-export const GET: APIRoute = async ({params, request}) => {
-   
+export const GET: APIRoute = async ({ request }) => {
   const posts = await getCollection("blog");
 
-  return new Response(JSON.stringify(posts), {
-    status: 201,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
+  const url = new URL(request.url);
+  console.log("URL:", url);
+  const slug = url.searchParams.get("slug");
+console.log("Slug:", slug);
+  if (slug) {
+    const post = await getEntry("blog", slug);
+    if (post) {
+      return new Response(JSON.stringify(post), {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+    } else {
+      return new Response(JSON.stringify({ error: "Post not found" }), {
+        status: 404,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+  } else {
+    return new Response(JSON.stringify(posts), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+};
