9ab992b7f07f8938722dd41b4ec8120eae40edca
Eric Durr Sierra <ericdurrsierra@Erics-MacBook-Pro.local>
2026-04-01 11:59:40 +0100

    add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 // update
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), db()],
   output: 'server',
   adapter: cloudflare(),
 });
